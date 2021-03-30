3c73221d82901f55eb57661f37fe3f6ae96ea222
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-03-30 00:32:32 -0300

    fix cors

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const cors = require("cors")
 const app = express();
 
 
-app.use(cors())
-//app.use(cors({origin: "https://applicationdatecontrol.herokuapp.com",}));
+//app.use(cors())
+app.use(cors({origin: "https://applicationdatecontrol.herokuapp.com",}));
 
 app.use(express.json());
 app.use(routes);
