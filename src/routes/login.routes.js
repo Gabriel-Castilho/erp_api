6301cd04a6b281bf210b37ef265e3925a131d494
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-08-11 12:39:45 -0300

    add base login

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -12,8 +12,8 @@ loginRouter.get("/",async (req,res)=>{
 
 loginRouter.post("/",async(req,res)=>{
     const{email,senha} = req.body
-    var salt = brycpt.genSaltSync(10);
-    var hash = bcrypt.hashSync(senha,salt);
+   // var salt = brycpt.genSaltSync(10);
+    //var hash = bcrypt.hashSync(senha,salt);
     const items = await loginController.create(email,senha)
     return res.json(items)
 })
