82502c0da67796f43877ad11b550432663666bd9
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-08-11 21:18:35 -0300

    Login

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,97 +1,104 @@
-const {Client}=require("pg");
+const { Client } = require("pg");
 const bcrypt = require('bcryptjs')
 
-class UsuarioController{
-  async index(){
-    try{
+class UsuarioController {
+  async index() {
+    try {
       const client = new Client({
-        connectionString:process.env.DATABASE_URL,
-        ssl:{
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
           rejectUnauthorized: false
-        },  
+        },
       });
       client.connect();
       const result = await client.query("SELECT * FROM public.usuarios;");
       client.end();
       const results = result.rows;
       return results;
-    }catch(err){
+    } catch (err) {
       console.error(err)
       return res.json(err)
     }
   }
 
-  async login(email,senha){
-    try{
+  async login(email, senha) {
+    try {
       const client = new Client({
-      connectionString:process.env.DATABASE_URL,
-      ssl:{
-        rejectUnauthorized: false
-      },  
-    });
-    client.connect();
-    const result = await client.query("SELECT * FROM public.usuarios WHERE email=$1;",[email])
-    if(result.rowCount  == 0){
-      const response = {
-        message:"Falha na autenticação"
-      }
-      return response
-
-    }else{
-      var corret = bcrypt.compareSync(senha,result.rows[0].senha)
-      if(corret){
-        const response = {
-          message:"Autenticado com Sucesso"
-        }
-        return response
-      }
-      client.end();
-    }
-   
-  }catch(err){
-    console.error(err)
-   const response={
-     message:"Error"
-   }
-  }
-  }
-  
-  async create(email,senha){
-    try{
-      const client = new Client({
-        connectionString:process.env.DATABASE_URL,
-        ssl:{
-          rejectUnauthorized:false
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
         },
       });
       client.connect();
-    //hash de senha
-     var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(senha,salt);
-    
+      const result = await client.query("SELECT * FROM public.usuarios WHERE email=$1;", [email])
+      client.end();
+      if (result.rowCount == 0) {
+        const response = {
+          message: "Falha na autenticação"
+        }
+        return response
+
+      } else {
+        var corret = bcrypt.compareSync(senha, result.rows[0].senha)
+        if (corret) {
+          const response = {
+            message: "Autenticado com Sucesso"
+          }
+          return response
+
+        } else {
+          const response = {
+            message: "Senha Errada"
+          }
+          return response
+        }
+      }
+
+
+    } catch (err) {
+      console.error(err)
+      const response = {
+        message: "Error"
+      }
+    }
+  }
+
+  async create(email, senha) {
+    try {
+      const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      });
+      client.connect();
+      //hash de senha
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(senha, salt);
+
       const result = await client.query("INSERT INTO public.usuarios (email,senha) VALUES($1, $2);",
-      [email,hash]);
+        [email, hash]);
       client.end();
       const results = result.rows;
       const response = {
-        message:"cadastrado"
+        message: "cadastrado"
       }
       return response;
-    }catch(err){
+    } catch (err) {
       console.error(err)
-      const response={
-        message:"Email já cadastrado"
+      const response = {
+        message: "Email já cadastrado"
       }
       return response;
     }
   }
 
-  async delete(id){
-    try{
+  async delete(id) {
+    try {
       const client = new Client({
-        connectionString:process.env.DATABASE_URL,
-        ssl:{
-          rejectUnauthorized:false
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
         },
       });
       client.connect();
@@ -99,54 +106,54 @@ class UsuarioController{
       client.end();
       const results = result.rows;
       return results;
-    }catch(err){
+    } catch (err) {
       console.error(err)
-      const response={
-        message:"erro ao deletar"
+      const response = {
+        message: "erro ao deletar"
       }
       return response
     }
   }
 
-  async getId(id){
-    try{
+  async getId(id) {
+    try {
       const client = new Client({
-        connectionString:process.env.DATABASE_URL,
-        ssl:{
-          rejectUnauthorized:false
-        }, 
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        },
       });
       client.connect();
       const result = await client.query("SELECT * FROM public.usuarios WHERE id=$1", [id]);
       client.end();
       const results = result.rows;
       return results;
-    }catch(err){
+    } catch (err) {
       console.error(err)
       return res.json(err)
     }
   }
 
-  async update(email,password){
-    try{
+  async update(email, password) {
+    try {
       const client = new Client({
-        connectionString:process.env.DATABASE_URL,
-        ssl:{
-          rejectUnauthorized:false
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
         },
       });
       client.connect();
-      const result = await client.query("UPDATE public.usuarios SET email=$1, password=$2 WHERE id=$3;",[email,password,id]);
+      const result = await client.query("UPDATE public.usuarios SET email=$1, password=$2 WHERE id=$3;", [email, password, id]);
       client.end();
       const results = result.rows;
       const response = {
-        message:"editado"
+        message: "editado"
       }
       return response;
-    }catch(err){
+    } catch (err) {
       console.error(err)
-      const response={
-        message:"erro"
+      const response = {
+        message: "erro"
       }
       return response;
     }
