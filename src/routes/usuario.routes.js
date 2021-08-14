97d32910086360df1be3251b694e75d828b4051f
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-08-14 13:45:33 -0300

    adicionando token

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -49,8 +49,8 @@ function auth(req,res,next){
     const authToken = req.headers['authorization']
     
     if(authToken != undefined){
-       const bearer = authToken.split('')
-       let token =  bearer[1]
+       const bearer = authToken.split(' ')
+       var token =  bearer[1]
        jwt.verify(token,jwtSecret,(err,data)=>{
            if(err){
                res.status(401)
