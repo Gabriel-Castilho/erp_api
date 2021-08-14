1226450d2e2b10198d7f1eeb289ca3038e78f3eb
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-08-14 13:34:59 -0300

    adicionando token

--- src/middleware/usuarioAuth.js
+++ src/middleware/usuarioAuth.js
@@ -1,25 +1,5 @@
-function usuarioAuth(req,res,next){
-    const authToken = req.headers['authorization']
+
     
-    if(authToken != undefined){
-       const bearer = authToken.split('')
-       let token =  bearer[1]
-       jwt.verify(token,jwtSecret,(err,data)=>{
-           if(err){
-               res.status(401)
-               res.json({err:"Token Inválido"})
-           }else{
-               req.token= token
-               req.loggedUder={id:data.id,email:data.email};
-               next()
-           }
-        
-       })
-    }else{
-      res.status(401);
-      res.json({err:"Token Inválido"})
-    }
   
-  }
-    module.exports = usuarioAuth()
+module.exports = usuarioAuth()
 
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,6 +1,5 @@
 const {Router} = require("express")
 const UsuarioController = require("../controllers/UsuarioController")
-const auth = require("../middleware/usuarioAuth")
 
 
 const usuarioRouter = Router();
@@ -43,6 +42,29 @@ usuarioRouter.patch("/:id",async(req,res)=>{
     const items = await usuarioController.update(email,senha,id)
     return res.json(items)
 })
+
+function auth(req,res,next){
+    const authToken = req.headers['authorization']
+    
+    if(authToken != undefined){
+       const bearer = authToken.split('')
+       let token =  bearer[1]
+       jwt.verify(token,jwtSecret,(err,data)=>{
+           if(err){
+               res.status(401)
+               res.json({err:"Token Inválido"})
+           }else{
+               req.token= token
+               req.loggedUder={id:data.id,email:data.email};
+               next()
+           }
+       })
+    }else{
+      res.status(401);
+      res.json({err:"Token Inválido"})
+    }
+  
+  }
 
 module.exports=usuarioRouter
 
